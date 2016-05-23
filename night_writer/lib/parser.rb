a449e44f6d988cac5edd2679ac1bc6e543542ad5
Angela Lindow <angelalindow@gmail.com>
2016-05-22 21:27:08 -0600

    building out the parser now

---
+++ night_writer/lib/parser.rb
@@ -0,0 +1,10 @@
+class Parser
+  attr_reader   :message
+  def initialize(message)
+    @message = message
+  end
+
+  def braille_array_parser
+    
+  end
+end
--- night_writer/test/parser_test.rb
+++ night_writer/test/parser_test.rb
@@ -7,9 +7,9 @@ require './lib/parser'
 
 class ParserTest < Minitest::Test
 
-  def test_can_parse_braille_into_3_arrays
-    conversion = Parser.new("0.....")
-    assert_equal [["0."],[".."],[".."]], conversion.parse
+  def test_can_parse_one_braille_character_into_3_arrays
+    conversion = Parser.new("0.....\n")
+    assert_equal [["0."],[".."],[".."]], conversion.braille_array_parser
   end
 
 end
